c74b4102cb611f57f79fdebbc63863817532df81
Raman Tank <ramantank04022002@gmail.com>
2024-10-30 16:07:23 +0530

    Initial Commit

---
+++ SwiftUICalculator/SwiftUICalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICalculatorApp.swift
+//  SwiftUICalculator
+//
+//  Created by Raman Tank on 30/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
